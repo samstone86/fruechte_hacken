ce1b6216fcc38573a05476a225caf9d88b932fc3
Philipp Vogel <philippwolf86.pw@gmail.com>
2019-06-25 21:23:51 +0200

    add fruit destroy physics
    add fractured apple

---
+++ Assets/Resources/Scripts/ExplodeFruitsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeFruitsScript : MonoBehaviour
+{
+    public float destroyDelay;
+    public float minForce;
+    public float maxForce;
+    public float radius;
+
+    void Start()
+    {
+        ExplodeFruits();
+    }
+
+    public void ExplodeFruits()
+    {
+        foreach (Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
+            }
+
+            Destroy(t.gameObject, destroyDelay);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/SpawnFracturedScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFracturedScript : MonoBehaviour
+{
+    public GameObject originalObject;
+    public GameObject fracturedObject;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnFracturedObject();
+        }
+    }
+
+    public void SpawnFracturedObject()
+    {
+        Destroy(originalObject);
+        GameObject fractObj = Instantiate(fracturedObject) as GameObject;
+        fractObj.GetComponent<ExplodeFruitsScript>().ExplodeFruits();
+    }
+}
