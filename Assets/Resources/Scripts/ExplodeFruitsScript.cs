e5bf712ac72e8a50c141d57e9d32d7ef64149ee6
Philipp Vogel <philippwolf86.pw@gmail.com>
2019-07-02 09:47:20 +0200

    add some particles when destroying a fruit

--- Assets/Resources/Scripts/ExplodeFruitsScript.cs
+++ Assets/Resources/Scripts/ExplodeFruitsScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ExplodeFruitsScript : MonoBehaviour
 {
+    public GameObject particleEffect;
+    public Vector3 particleEffectOffset;
     public float destroyDelay;
     public float minForce;
     public float maxForce;
@@ -11,6 +13,12 @@ public class ExplodeFruitsScript : MonoBehaviour
 
     public void ExplodeFruits()
     {
+        if(particleEffect != null)
+        {
+            GameObject particleEffectFX = Instantiate(particleEffect, transform.position + particleEffectOffset, Quaternion.identity) as GameObject;
+            Destroy(particleEffectFX, 5);
+        }
+
         foreach (Transform t in transform)
         {
             var rb = t.GetComponent<Rigidbody>();
